3685f0caa94c7e33577c82a827fbfb8c0300ad1c
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:10:54 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,3 @@
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 git clone https://github.com/vcrhonek/hwdata -b v0.370
 sed -i 's#/usr/bin/env\ python#/usr/bin/env\ python3#' ./*

5ba73f41cd105e5578b25a4c550f448beb705118
ferrreo <harderthanfire@gmail.com>
2023-08-30 20:24:59 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ dpkg-sig --sign builder ./output/hwdata*.deb
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb lunar ./output/hwdata*.deb
+reprepro -V -C main --basedir ./output/repo/ includedeb lunar ./output/hwdata*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
